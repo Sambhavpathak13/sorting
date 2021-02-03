51d2b94b3c9b4ab31eafd748619df3367c7b0614
Sambhav <Sambhav@DESKTOP-8OK74VJ>
2021-02-03 10:07:58 +0530

    Bubble Sort

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,14 @@
+package sorting;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bubble sort Begin");
+		int a=30;
+		System.out.println(a);
+		System.out.println("Bubble Sort Ends");
+
+	}
+
+}
